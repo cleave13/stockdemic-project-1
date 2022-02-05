deef6732bfdfbcf6a356b0596e9b32fb5abb0a4a
christierney3 <chris.tierney3@gmail.com>
2022-02-05 12:04:50 -0700

    added variables and current date to js

--- assetts/app.js
+++ assetts/app.js
@@ -1,7 +1,18 @@
 // Establish variables
-var apiKey = 'DVNcrtS4FS68E1LAklJ6h7MWq7aEdRbLXehTvdG8';
-var yahooUrl = 'https://yfapi.net/v8/finance/spark?interval=1d&range=1mo&symbols=AAPL&appid=' + apiKey;
-var covidUrl = 'https://covid-api.mmediagroup.fr/v1/history?country=US&status=confirmed';
+var header = document.querySelector('.title-flex-container');
+var headTitle = document.querySelector('.title');
+var dateContainer = document.querySelector('.date-flex-container');
+var startDateInput = document.getElementById('Start');
+var endDateInput = document.getElementById('End');
+var caseNum = document.querySelector('.pandemic-info');
+var stockData = document.querySelector('.stock-info');
+var graph = document.querySelector('.comparison-graph');
+var emailInput = document.getElementById('email-input');
+var timeDisplay = document.getElementById('current-time');
+// API keys/URLs
+var apiKey = 'DVNcrtS4FS68E1LAklJ6h7MWq7aEdRbLXehTvdG8'
+var yahooUrl = 'https://yfapi.net/v8/finance/spark?interval=1d&range=1mo&symbols=AAPL&appid=' + apiKey
+var covidUrl = 'https://covid-api.mmediagroup.fr/v1/history?country=US&status=confirmed'
 
 // Call on our APIs
 fetch(yahooUrl, {
@@ -29,14 +40,35 @@ fetch(covidUrl, {
     console.log(data)
 })
 
+// Create functions to update time by the second
+function updateTime () {
+    var refresh = 1000;
+    myTime=setTimeout('displayTime()', refresh);
+}
+
+function displayTime() {
+    var currentTime = dayjs();
+    timeDisplay.textContent = currentTime;
+    updateTime();
+}
+
+displayTime();
+
+
+
+function saveStartDate() {
+    localStorage.setItem('Start', startDateInput.value);
+}
+
+function saveEndDate() {
+    localStorage.setItem('End', endDateInput.value);
+}
+
 // (optional) Use moment to display current time
 
 
 // Save fetched data to local storage
-function saveLocal() {
-    localStorage.setItem('Price', price);
-    localStorage.setItem('Cases', cases);
-}
+
 
 // Populate certain data to respective areas of page
 
@@ -51,3 +83,10 @@ function saveLocal() {
 
 
 // Add event listeners to buttons/submits
+startDateInput.onchange = function () {
+    saveStartDate();
+}
+
+endDateInput.onchange = function () {
+    saveEndDate();
+}
